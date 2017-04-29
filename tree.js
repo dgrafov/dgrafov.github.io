b2e28f156ae9ff5aac2be049dfdb68de391f22c4
Denis Grafov <grafovdv@gmail.com>
2017-04-29 23:24:46 +0200

    Finished tree building

--- tree.js
+++ tree.js
@@ -1,134 +1,356 @@
+var noFunctionError = "Введите функцию";
+var unknownFunction = "Неизвестная функция: "
+var unknownSymbol = "Неизвестный символ: "
+var noClosingBracket = "Не хватает закрывающей скобки"
+var noOpeningBracket = "Не хватает открывающей скобки"
+
+function loadExample(exampleId) {
+    document.getElementById('function').value = document.getElementById(exampleId).innerHTML;
+}
+
+function showCalculate() {
+    hideTree();
+    hideError();
+    var input = document.getElementById('function').value;
+
+    if(input.length > 0) {
+        console.log("calculate: " + input);
+        //Do smth
+    }
+    else {
+        displayError(noFunctionError);
+    }
+}
+
+function showBuildTree() {
+    hideTree();
+    hideError();
+    var input = document.getElementById('function').value;
+    if(input.length > 0) {
+        console.log("buildTree:" + input);
+        var ret = buildTree(input);
+        if(ret[0] === true) {
+            drawTree(ret[1]);
+        }
+        else {
+            displayError(ret[1]);
+        }
+    }
+    else {
+        displayError(noFunctionError);
+    }
+}
+
+function showSupportedSymbols() {
+    document.getElementById('supportedSymbols').style.display = "block";
+    var button = document.getElementById('supportedSymbolsButton');
+    button.onclick = hideSupportedSymbols;
+    button.innerHTML = "Скрыть";
+}
+
+function hideSupportedSymbols() {
+    document.getElementById('supportedSymbols').style.display = "none";
+    var button = document.getElementById('supportedSymbolsButton');
+    button.onclick = showSupportedSymbols;
+    button.innerHTML = "Показать";
+}
+
+function hideTree() {
+    document.getElementById('treeDiv').style.display = "none";
+}
+
+function hideError() {
+    document.getElementById('errorDiv').style.display = "none";
+}
+
+function buildTree(input) {
+    input = input.split(' ').join('');
+    var opStack = [];
+    var outStack = [];
+    var error = "";
+
+    var i = 0;
+    while(i < input.length && error === "") {
+        if(isOperator(input[i])) {
+            if((i == 0 || input[i - 1] === '(' || isOperator(input[i - 1]))
+                && input[i] === '-') {
+                // unary minus
+                var op = new Operator(input[i], true, false);
+            }
+            else{
+                var op = new Operator(input[i], false, false);
+            }
+
+            while(opStack.length > 0
+                && opStack[opStack.length - 1] !== '('
+                && op.prio <= opStack[opStack.length - 1].prio)
+            {
+                popOperatorFromStack(opStack, outStack);
+            }
+            opStack.push(op);
+            i++;
+        }
+
+        else if(input[i] === '(') {
+            opStack.push(input[i]);
+            i++;
+        }
+
+        else if(input[i] === ')') {
+
+            while(opStack.length > 0 && opStack[opStack.length - 1] !== '(')
+            {
+                popOperatorFromStack(opStack, outStack);
+            }
+
+            if(opStack.length > 0 && opStack[opStack.length - 1] === '(') {
+                opStack.pop();
+                if(opStack.length > 0 && opStack[opStack.length - 1].func === true) {
+                    popOperatorFromStack(opStack, outStack);
+                }
+            }
+            else {
+                error = noOpeningBracket;
+            }
 
 
-/*
+            i++;
+        }
 
-sqrt(x)
-rt(n, x)
-sin
-cos
-tg
-ctg
-e^x
-a^x
-ln(x)
-log(a, x)
+        else if(input[i] === ',') {
 
-удалить пробелы
-обработка ошибок (пустой стек)
-*/
-window.onload = function() {
-	document.getElementById('out').innerHTML = 'Hello JavaScript!'
-	var input = "(-3*(x+1)-8/(-x))/(x^(2+k)+x)";
-	var opStack = [];
-	var outStack = [];
+            while(opStack.length > 0 && opStack[opStack.length - 1] !== '(')
+            {
+                popOperatorFromStack(opStack, outStack);
+            }
 
-	for(i in input) {
-		console.log("Token: " + input[i]);
+            if(opStack.length == 0) {
+                error = noOpeningBracket;
+            }
+            i++;
+        }
 
-		if(isOperator(input[i])) {
-			if((i == 0 || input[i - 1] === '(') && input[i] === '-') {
-				// unary minus
-				var op = new Operator(input[i], true);
-			}
-			else{
-				var op = new Operator(input[i], false);
-			}
+        else if (isLetter(input[i])){
+            // operand or function
+            var n = i;
+            var str = "";
+            do {
+                str += input[n];
+                n++;
+            } while (n < input.length
+                &&  (isLetter(input[n]) || input[n] === '.' ));
 
-			console.log("Operator (" + op.prio + ")");
+            if(str.length === 1) {
+                //operand
+                outStack.push(new Node(str));
+            }
+            else {
+                var op = processFunction(str);
+                if(op !== null) {
+                    opStack.push(op);
+                }
+                else{
+                    //error unknown function
+                    error = unknownFunction + str;
+                }
+            }
+            i = n;
+        }
 
-			while(opStack.length > 0
-				&& opStack[opStack.length - 1] !== '('
-				&& op.prio <= opStack[opStack.length - 1].prio)
-			{
-				popOperatorFromStack(opStack, outStack);
-			}
-			opStack.push(op);
-		}
+        else if (isDigit(input[i])) {
+            // numeric operand
+            var n = i;
+            var numStr = "";
+            do {
+                numStr += input[n];
+                n++;
+            } while (n < input.length
+                &&  (isDigit(input[n]) || input[n] === '.' ));
+            outStack.push(new Node(parseFloat(numStr)));
+            i = n;
+        }
+        else {
+            //error
+            error = unknownSymbol + input[i];
+            console.log(error);
+            break;
+        }
+    }
+    if(error === "") {
+        while(opStack.length > 0 && error === "") {
+            if(opStack[opStack.length - 1].token == '(' ) {
+                error = noClosingBracket;
+            }
+            else {
+                popOperatorFromStack(opStack, outStack);
+            }
 
-		else if(input[i] === '(') {
-			console.log("Opening bracket");
-			opStack.push(input[i]);
-		}
+        }
+        return [true, outStack[0]];
+    }
 
-		else if(input[i] === ')') {
-			console.log("Closing bracket");
+    return [false, error];
+}
 
-			while(opStack.length > 0 && opStack[opStack.length - 1] !== '(')
-			{
-				popOperatorFromStack(opStack, outStack);
-			}
+function isLetter(c) {
+  return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+}
 
-			if(opStack.length > 0 && opStack[opStack.length - 1] === '(') {
-				opStack.pop();
-			}
+function isDigit(c) {
+  return c >= '1' && c <= '9';
+}
 
-		}
-		else {
-			console.log("Operand");
-			outStack.push(new Node(input[i]));
-		}
-	}
-
-	while(opStack.length > 0)
-	{
-		popOperatorFromStack(opStack, outStack);
-	}
-
-	console.log("outStack:" + outStack.length);
-	printTree(outStack[0]);
-
-
+function processFunction(str) {
+    str = str.toLowerCase();
+    var unary = false;
+    switch(str) {
+    case "sqrt":
+    case "sin":
+    case "cos":
+    case "tg":
+    case "ctg":
+    case "ln":
+        unary = true;
+        break;
+    case "rt":
+    case "log":
+        unary = false;
+        break;
+    default:
+        console.log("Unknown function: " + str);
+        return null;
+    }
+    return new Operator(str, unary, true);
 }
 
 function popOperatorFromStack(operatorStack, outputStack) {
-	var operator = operatorStack.pop();
-	var node = new Node(operator.token);
-	if (operator.unary === false) {
-		node.right = outputStack.pop();
-		node.right.parent = node;
-	}
+    var operator = operatorStack.pop();
+    var node = new Node(operator.token);
+    if (operator.unary === false) {
+        node.right = outputStack.pop();
+        node.right.parent = node;
+    }
 
-	node.left = outputStack.pop();
-	node.left.parent = node;
+    node.left = outputStack.pop();
+    node.left.parent = node;
 
-	outputStack.push(node);
+    outputStack.push(node);
 }
 
 function printTree(root) {
-	var queue = [];
-	queue.push(null);
-	queue.push(root);
-	var str = "";
-	while(queue.length > 0) {
-		var cur = queue.shift();
-		if(cur === null) {
-			if (queue.length > 0) {
-				queue.push(null);
-				str += '\n';
-			}
-		}
-		else {
-			str += cur.token + " ";
-			if (cur.token !== "X") {
-				if(cur.left !== null) {
-					queue.push(cur.left);
-				}
-				else {
-					queue.push(new Node("X"));
-				}
+    var queue = [];
+    queue.push(null);
+    queue.push(root);
+    var str = "";
+    while(queue.length > 0) {
+        var cur = queue.shift();
+        if(cur === null) {
+            if (queue.length > 0) {
+                queue.push(null);
+                str += '\n';
+            }
+        }
+        else {
+            str += cur.token + " ";
+            if (cur.token !== "X") {
+                if(cur.left !== null) {
+                    queue.push(cur.left);
+                }
+                else {
+                    queue.push(new Node("X"));
+                }
 
-				if(cur.right !== null) {
-					queue.push(cur.right);
-				}
-				else {
-					queue.push(new Node("X"));
-				}
-			}
+                if(cur.right !== null) {
+                    queue.push(cur.right);
+                }
+                else {
+                    queue.push(new Node("X"));
+                }
+            }
 
-		}
-	}
-	console.log(str);
+        }
+    }
+    console.log(str);
+}
 
+function drawTree(root) {
+    document.getElementById('treeDiv').style.display = "block";
+    var canvas = document.getElementById('treeCanvas');
+
+    var nodeRadPx = 12;
+
+    var height = getTreeHeight(root);
+    var width = Math.pow(2, height - 1);
+
+    var heightPx = nodeRadPx * 2 * height * 2;
+    var widthPx =  nodeRadPx * 2 * width * 2;
+    console.log("width: " + widthPx + ", height: " + heightPx);
+    canvas.width = widthPx;
+    canvas.height = heightPx;
+
+    drawSubTree(root, canvas, 0, widthPx, nodeRadPx + 1, nodeRadPx);
+}
+
+function displayError(msg) {
+    document.getElementById('errorDiv').style.display = "block";
+    document.getElementById('errorText').innerHTML = msg;
+}
+
+function drawSubTree(root, canvas, xMin, xMax, y, nodeRadPx) {
+        var ctx = canvas.getContext("2d");
+
+        var x = ( xMax - xMin ) / 2 + xMin;
+
+        var newY = y + nodeRadPx * 3;
+        //draw lines
+        if(root.left !== null) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(( x - xMin ) / 2 + xMin, newY);
+            ctx.stroke();
+
+        }
+        if(root.right !== null) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(( xMax - x ) / 2 + x, newY);
+            ctx.stroke();
+        }
+
+        //draw circle and fill
+        ctx.beginPath();
+        ctx.arc(x, y, nodeRadPx, 0, 2*Math.PI);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+        ctx.stroke();
+
+        //draw text
+        ctx.font = "12px Arial";
+        ctx.fillStyle = 'black';
+        ctx.textAlign = 'center';
+        ctx.fillText(root.token, x, y + nodeRadPx / 2);
+
+        //draw children
+        if(root.left !== null) {
+            drawSubTree(root.left, canvas, xMin, x, newY, nodeRadPx);
+        }
+        if(root.right !== null) {
+            drawSubTree(root.right, canvas, x, xMax, newY, nodeRadPx);
+        }
+}
+
+function getTreeHeight(root) {
+    if (root === null) {
+        return 0;
+    }
+    var leftH = getTreeHeight(root.left);
+    var rightH = getTreeHeight(root.right);
+
+    if (leftH > rightH) {
+        return leftH + 1;
+    } else {
+        return rightH + 1;
+    }
 }
 
 function Node(token) {
@@ -138,37 +360,40 @@ function Node(token) {
     this.token = token;
 }
 
-function Operator(token, unary) {
+function Operator(token, unary, func) {
     this.token = token;
     //prio
     this.prio = 0;
     //type (binary/unary)
-    this.unary = false;
-   	if(unary) {
-   		this.unary = true;
-   		// only minus here
-   		this.prio = 4;
-   	}
-   	else {
-   		// binary operators
-   		if(token === '+' || token === '-' ) {
-    		this.prio = 3;
-    	}
-    	else if (token === '*' || token === '/' ) {
-    		this.prio = 5;
-    	}
-    	else if (token === '^') {
-    		this.prio = 6;
-    	}
-   	}
-
+    this.unary = unary;
+    this.func = func;
+    this.prio = 0;
+    // priority - not needed for functions
+    if(!this.func) {
+        if(this.unary) {
+            // only minus here
+            this.prio = 5;
+        }
+        else {
+            // binary operators
+            if(token === '+' || token === '-' ) {
+                this.prio = 3;
+            }
+            else if (token === '*' || token === '/' ) {
+                this.prio = 4;
+            }
+            else if (token === '^') {
+                this.prio = 6;
+            }
+        }
+    }
 }
 
 function isOperator(token) {
-	if( token === '+' || token === '-' || token === '*' || token === '/' || token === '^') {
-		return true;
-	}
-	return false;
+    if( token === '+' || token === '-' || token === '*' || token === '/' || token === '^') {
+        return true;
+    }
+    return false;
 }
 
 
