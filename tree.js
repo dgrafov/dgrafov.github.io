bd86b4f5b47f92f2cbd8de3b1a9f2c72fc86044c
Denis Grafov <grafovdv@gmail.com>
2017-06-11 17:17:37 +0200

    Store last calculated function

--- tree.js
+++ tree.js
@@ -28,27 +28,36 @@ var noOpeningBracket = "Не хватает открывающей скобки"
 //TODO Fix a stupid approach of having only left child for unary operators and functions. It should be left for post-operator
 //and right - for pre-operator
 
+//TODO bugs: x+, (x+1
+
 var originalTree = null;
 var derivativeTree = null;
+var lastInput = null;
 
 function loadExample(exampleId) {
     document.getElementById('function').value = document.getElementById(exampleId).innerHTML;
 }
 
 function calculateDerivative() {
-
     var input = document.getElementById('function').value;
 
     if(input.length > 0) {
-        var ret = buildTree(input);
-        if(ret[0] === true) {
-            derivativeTree = simplify(derivative(ret[1]));
-            originalTree = ret[1];
+        input = input.split(' ').join('');
 
-            displayResult(buildExpression(derivativeTree));
-        }
-        else {
-            displayError(ret[1]);
+        if(lastInput !== input) {
+            lastInput = input;
+            originalTree = null;
+            derivativeTree = null;
+            var ret = buildTree(input);
+            if(ret[0] === true) {
+                derivativeTree = simplify(derivative(ret[1]));
+                originalTree = ret[1];
+
+                displayResult(buildExpression(derivativeTree));
+            }
+            else {
+                displayError(ret[1]);
+            }
         }
     }
     else {
@@ -119,11 +128,15 @@ function drawTree(root, divId, canvasId) {
 function displayError(msg) {
     document.getElementById('errorDiv').style.display = "block";
     document.getElementById('errorText').innerHTML = msg;
+    hideResult();
 }
 
 function displayResult(res) {
     document.getElementById('difDiv').style.display = "block";
     document.getElementById('result').innerHTML = res;
+    hideOriginalTree();
+    hideDerivativeTree();
+    hideError();
 }
 
 function showSupportedSymbols() {
@@ -145,9 +158,14 @@ function hideError() {
     document.getElementById('errorDiv').style.display = "none";
 }
 
+function hideResult() {
+    document.getElementById('difDiv').style.display = "none";
+    hideDerivativeTree();
+    hideOriginalTree();
+}
+
 
 function buildTree(input) {
-    input = input.split(' ').join('');
     var opStack = [];
     var outStack = [];
     var error = "";
